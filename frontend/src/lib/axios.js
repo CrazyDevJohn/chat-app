8fa84a6f967360b87d177dacb165040058ce1f45
Crazy Dev John <56544124+CrazyDevJohn@users.noreply.github.com>
2026-02-04 18:40:36 +0530

    Update axios base URL for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "/api",
+      : "https://chat-app-z6qd-git-main-crazydevjohns-projects.vercel.app/api",
   withCredentials: true,
   // headers: {
   //   "Content-Type": "application/json;charset=utf-8",
