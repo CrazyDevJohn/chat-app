79ac6558b8a57f9df8c8adcf81f4d6f8705e2704
Crazy Dev John <56544124+CrazyDevJohn@users.noreply.github.com>
2026-02-04 18:44:19 +0530

    Fix API URL for production environment

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-z6qd-git-main-crazydevjohns-projects.vercel.app/";
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-z6qd-git-main-crazydevjohns-projects.vercel.app/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
